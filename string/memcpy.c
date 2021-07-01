49616906b59add33c5f6e5de7cf1b6a10d77c240
Ferns <jeffreyzhang123@gmail.com>
2021-07-01 16:53:42 -0700

    Optimizations

--- string/memcpy.c
+++ string/memcpy.c
@@ -1,5 +1,13 @@
 #include "../string.h"
 
 void *memcpy(void *restrict dest, const void *restrict src, unsigned int n) {
-	
+	unsigned char *d = dest;
+	const unsigned char *s = src;
+
+	while(n) {
+		*d++ = *s++;
+		n--;
+	}
+
+	return dest;
 }
---
+++ string/memset.c
@@ -0,0 +1,32 @@
+#include "../string.h"
+
+void *memset(void *dest, int c, unsigned int n) {
+	unsigned int val = (c << 24) | ((c & 0xFF) << 16) | ((c & 0xFF) << 8) | (c & 0xFF);
+	unsigned char *d = dest;
+	unsigned int i = 0;
+	unsigned int l = n >> 2;
+
+	while(i < l) {
+		((unsigned int *) d)[i] = val;
+		i += 1;
+	}
+
+	n = n - (i << 2);
+
+	switch(n) {
+		case 1: 
+			*((unsigned char *) d) = val;
+			break;
+		case 2:
+			*((unsigned short *) d) = val;
+			break;
+		case 3:
+			*((unsigned short *) d++) = val;
+			*((unsigned char *) d) = val;
+			break;
+		default:
+			break;
+	}
+
+	return dest;
+}
