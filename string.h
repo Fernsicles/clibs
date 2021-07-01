abfffcd3faa25f868cba96c0bb385675612347d6
Ferns <jeffreyzhang123@gmail.com>
2021-06-30 22:30:36 -0700

    memcpy to begin with

---
+++ string.h
@@ -0,0 +1,6 @@
+#ifndef _STRING_H
+#define _STRING_H
+#define NULL ((void *) 0)
+
+void *memcpy(void *__restrict, const void *__restrict, unsigned int n);
+#endif
---
+++ string/memcpy.c
@@ -0,0 +1,5 @@
+#include "../string.h"
+
+void *memcpy(void *restrict dest, const void *restrict src, unsigned int n) {
+	
+}
